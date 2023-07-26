2a0fd30049e56c0b7320b3d4b6e9b2f08c327c24
Víctor Carmona <vicarali@gmail.com>
2023-07-26 19:05:08 +0200

    Set up service cards animation using observer

---
+++ version_control/katalyst_by_codurance/modules/Services-Cards.module/module.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const serviceListing = document.querySelector(".services-cards");
+const serviceCards = serviceListing.querySelectorAll(".service-cards__card");
+
+serviceCards.forEach((card) => {
+  const graphic = card.querySelector("svg");
+
+  const observer = new IntersectionObserver(
+    () => {
+      graphic.animate([{ opacity: 0 }, { opacity: 1 }], 1000);
+    },
+    { threshold: 1.0 }
+  );
+
+  observer.observe(card);
+});

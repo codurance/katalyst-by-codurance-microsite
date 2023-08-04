df22e724e9179fdb574ec7ba37267a9b44f3705b
Víctor Carmona <vicarali@gmail.com>
2023-08-04 15:28:01 +0200

    Set up parallax effect on hero banner

--- version_control/katalyst_by_codurance/js/main.js
+++ version_control/katalyst_by_codurance/js/main.js
@@ -1,3 +1,39 @@
 "use strict";
 
-(function() {})();
+window.addEventListener("DOMContentLoaded", () => {
+  const heroImageCanvas = document.querySelector(".hero-banner__image");
+  const heroImage = heroImageCanvas.querySelector("img");
+
+  const CANVAS_SIZE = parseInt(
+    getComputedStyle(heroImageCanvas).getPropertyValue("--hero-image-height")
+  );
+
+  const observer = new IntersectionObserver(() => {
+    heroImageCanvas.classList.toggle("visible");
+  });
+
+  observer.observe(heroImageCanvas);
+
+  window.addEventListener("scroll", () => {
+    applyParallaxEffect();
+  });
+
+  function applyParallaxEffect() {
+    const imageScroll = window.scrollY / 2; //scroll halved to slow parallax
+
+    if (scrollingHeroImage(imageScroll)) {
+      heroImageCanvas.style.setProperty("--scroll", imageScroll + "px");
+    }
+  }
+
+  function scrollingHeroImage(imageScroll) {
+    return (
+      heroImageCanvas.classList.contains("visible") &&
+      imageScroll < calculateSpaceBetweenImageAndCanvas()
+    );
+  }
+
+  function calculateSpaceBetweenImageAndCanvas() {
+    return CANVAS_SIZE - heroImage.offsetHeight;
+  }
+});

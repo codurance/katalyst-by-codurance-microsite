880096075f3d072fbacf5f9bef0e37b074e41c36
Víctor Carmona <vicarali@gmail.com>
2023-08-07 17:16:39 +0200

    Set up fade-in animations for sections with cards

--- version_control/katalyst_by_codurance/js/main.js
+++ version_control/katalyst_by_codurance/js/main.js
@@ -1,39 +1,26 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded", () => {
-  const heroImageCanvas = document.querySelector(".hero-banner__image");
-  const heroImage = heroImageCanvas.querySelector("img");
+const observer = new IntersectionObserver(fadeInOnIntersection);
 
-  const CANVAS_SIZE = parseInt(
-    getComputedStyle(heroImageCanvas).getPropertyValue("--hero-image-height")
-  );
-
-  const observer = new IntersectionObserver(() => {
-    heroImageCanvas.classList.toggle("visible");
-  });
-
-  observer.observe(heroImageCanvas);
-
-  window.addEventListener("scroll", () => {
-    applyParallaxEffect();
-  });
-
-  function applyParallaxEffect() {
-    const imageScroll = window.scrollY / 2; //scroll halved to slow parallax
-
-    if (scrollingHeroImage(imageScroll)) {
-      heroImageCanvas.style.setProperty("--scroll", imageScroll + "px");
+function fadeInOnIntersection(entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        [
+          { opacity: 0, filter: "blur(5px)" },
+          { opacity: 1, filter: "blur(0px)" }
+        ],
+        500
+      );
+      observer.unobserve(entry.target);
     }
-  }
+  });
+}
 
-  function scrollingHeroImage(imageScroll) {
-    return (
-      heroImageCanvas.classList.contains("visible") &&
-      imageScroll < calculateSpaceBetweenImageAndCanvas()
-    );
-  }
+window.addEventListener("DOMContentLoaded", () => {
+  const fadeInElements = document.querySelectorAll(".fade-in");
 
-  function calculateSpaceBetweenImageAndCanvas() {
-    return CANVAS_SIZE - heroImage.offsetHeight;
-  }
+  fadeInElements.forEach((element) => {
+    observer.observe(element);
+  });
 });

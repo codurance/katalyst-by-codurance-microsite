b9cd1f10b60b6b5644b95d705933ff4a44454221
Víctor Carmona <vicarali@gmail.com>
2023-08-10 14:11:56 +0200

    Remove blur effect from fade-in animations

--- version_control/katalyst_by_codurance/js/main.js
+++ version_control/katalyst_by_codurance/js/main.js
@@ -5,13 +5,7 @@ const observer = new IntersectionObserver(fadeInOnIntersection);
 function fadeInOnIntersection(entries, observer) {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.animate(
-        [
-          { opacity: 0, filter: "blur(5px)" },
-          { opacity: 1, filter: "blur(0px)" }
-        ],
-        500
-      );
+      entry.target.animate([{ opacity: 0 }, { opacity: 1 }], 500);
       observer.unobserve(entry.target);
     }
   });

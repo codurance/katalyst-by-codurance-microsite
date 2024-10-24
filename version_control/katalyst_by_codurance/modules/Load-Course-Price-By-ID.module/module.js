7817376289f731923c3dd41dac82f043a483f9df
Benjamin Rae <benjaminrae93@gmail.com>
2024-10-24 13:49:59 +0200

    Use new price format from payment service

--- version_control/katalyst_by_codurance/modules/Load-Course-Price-By-ID.module/module.js
+++ version_control/katalyst_by_codurance/modules/Load-Course-Price-By-ID.module/module.js
@@ -5,20 +5,40 @@ setCoursePrice();
 async function setCoursePrice() {
   let coursePrice = await fetchCoursePrice();
 
-  if (!coursePrice) {
-    coursePrice = 14.99
-  }
-
   const coursePriceTags = document.querySelectorAll(".price");
 
   coursePriceTags.forEach((coursePriceTag) => {
     const priceNumberTag = coursePriceTag.querySelector(".course-price-js");
-    priceNumberTag.textContent = coursePrice;
+    const priceBaseTag = coursePriceTag.querySelector(".course-price-base");
+    const priceSymbolTag = coursePriceTag.querySelector(".course-price-symbol");
+
+    priceSymbolTag.textContent = coursePrice.currency.symbol;
+
+    if (coursePrice.discountPrice === undefined || coursePrice.discountPrice === null) {
+      priceNumberTag.textContent = coursePrice.basePrice.toString();
+    } else {
+      priceNumberTag.textContent = coursePrice.discountPrice.toString();
+      priceBaseTag.textContent = coursePrice.basePrice.toString();
+    }
 
     coursePriceTag.classList.add("price--shown");
   });
 }
 
+/**
+ * @returns {Promise<{country: string, basePrice: number, discountPrice: number, currency: {iso: string, symbol: string}}>}
+ * @example
+ * // Example of a resolved Promise
+ * // {
+ * //   country: "Spain",
+ * //   basePrice: 60.0,
+ * //   discountPrice: 14.99,
+ * //   currency: {
+ * //     iso: "EUR",
+ * //     symbol: "€"
+ * //   }
+ * // }
+ */
 async function fetchCoursePrice() {
   const courseData = JSON.parse(
     document.getElementById("course-data").textContent
